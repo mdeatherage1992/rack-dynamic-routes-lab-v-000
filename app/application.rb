0735acb717d6e5c82334cf340247771f420bc5c1
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-10 14:01:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
     item = req.path.split("/items/").last
-      if item = @@items.find{|i| i.name == item.name}
+      if item = @@items.find{|i| i.name == item}
         resp.write item.price
       else
         resp.stauts = 400
